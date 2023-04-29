464bc5fa3286e6d73d26b9cd97f03c8495ed3922
robertr-dev <jmatt03@gmail.com>
2023-04-29 16:28:30 -0400

    main update

--- VR_Project3/Assets/FadeScreen.cs
+++ VR_Project3/Assets/FadeScreen.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FadeScreen : MonoBehaviour
 {
     public bool fadeOnStart = true;
-    public float fadeDuration = 2;
+    public float fadeDuration = 11;
     public Color fadeColor;
     private Renderer rend;
 
